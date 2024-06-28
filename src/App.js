ff7903ba6aa6c502a2e91179b026bf86a6eccfc6
torihazi <lovebarusa19981225@icloud.com>
2024-06-28 23:47:33 +0900

    fin react_todo_list
    add components and AlertDialogModal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import {
-  Button,
-  ChakraProvider,
-  Box,
-  Heading,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  ListItem,
-  VStack,
-  Checkbox,
-  List,
-  ButtonGroup,
-} from "@chakra-ui/react";
+import { ChakraProvider, VStack } from "@chakra-ui/react";
 
-import { CheckIcon } from "@chakra-ui/icons";
 import "./App.css";
 import { useState } from "react";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 
 function App() {
   const [todoText, setTodoText] = useState("");
@@ -106,111 +95,24 @@ function App() {
   return (
     <ChakraProvider>
       <VStack spacing={10}>
-        <Box>
-          <Heading textAlign="center" as="h1" size="2xl" mb={4}>
-            Todoリスト
-          </Heading>
-          <InputGroup>
-            <InputLeftElement>
-              <CheckIcon color="teal.300" />
-            </InputLeftElement>
-            <Input
-              htmlSize={30}
-              variant="outline"
-              placeholder="本日やるべきこと"
-              value={todoText}
-              onChange={onChangeText}
-            />
-            <Button colorScheme="teal" onClick={onClickAddTodo}>
-              Todo
-            </Button>
-          </InputGroup>
-        </Box>
-        <Box
-          minH="200px"
-          minW="500px"
-          border="1px"
-          borderRadius="10"
-          borderColor="gray.100"
-          p={5}
-        >
-          <Heading textAlign="center" as="h2" size="lg">
-            未完了リスト
-          </Heading>
-          <List spacing={2}>
-            {incompleteTodos.map((todo, index) => (
-              <ListItem
-                display="flex"
-                justifyContent="space-between"
-                key={index}
-              >
-                {todo.isEditable ? (
-                  <>
-                    <Input
-                      htmlSize={30}
-                      variant="outline"
-                      value={todo.text}
-                      onChange={(e) => onChangeUpdateText(e, index)}
-                    />
-                    <Button onClick={() => onClickUpdate(index)}>更新</Button>
-                  </>
-                ) : (
-                  <>
-                    <Checkbox
-                      isChecked={todo.isChecked}
-                      onChange={() => onChangeComplete(index)}
-                    >
-                      {todo.text}
-                    </Checkbox>
-                    <ButtonGroup>
-                      <Button onClick={() => onClickToggleUpdate(index)}>
-                        編集
-                      </Button>
-                      <Button
-                        onClick={() => onClickDeleteIncompleteTodo(index)}
-                      >
-                        削除
-                      </Button>
-                    </ButtonGroup>
-                  </>
-                )}
-              </ListItem>
-            ))}
-          </List>
-        </Box>
-        <Box
-          minH="200px"
-          minW="500px"
-          border="1px"
-          borderRadius="10"
-          borderColor="gray.100"
-          p={5}
-        >
-          <Heading textAlign="center" as="h2" size="lg">
-            完了リスト
-          </Heading>
-          <List spacing={2}>
-            {completeTodos.map((todo, index) => (
-              <ListItem
-                display="flex"
-                justifyContent="space-between"
-                key={index}
-              >
-                <Checkbox
-                  isChecked={todo.isChecked}
-                  onChange={() => onChangeBack(index)}
-                >
-                  {todo.text}
-                </Checkbox>
-                <ButtonGroup>
-                  <Button onClick={() => onClickDeleteCompleteTodo(index)}>
-                    削除
-                  </Button>
-                </ButtonGroup>
-              </ListItem>
-            ))}
-          </List>
-        </Box>
+        <InputTodo
+          todoText={todoText}
+          onClick={onClickAddTodo}
+          onChange={onChangeText}
+        />
+        <IncompleteTodos
+          incompleteTodos={incompleteTodos}
+          onChangeUpdateText={onChangeUpdateText}
+          onClickUpdate={onClickUpdate}
+          onChangeComplete={onChangeComplete}
+          onClickToggleUpdate={onClickToggleUpdate}
+          onClickDeleteIncompleteTodo={onClickDeleteIncompleteTodo}
+        />
+        <CompleteTodos
+          completeTodos={completeTodos}
+          onChangeBack={onChangeBack}
+          onClickDeleteCompleteTodo={onClickDeleteCompleteTodo}
+        />
       </VStack>
     </ChakraProvider>
   );
