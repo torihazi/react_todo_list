92bdf9872ada89e724085d3affa9b700a77c2c77
torihazi <lovebarusa19981225@icloud.com>
2024-07-02 09:41:20 +0900

    create CustomHooks

--- src/App.js
+++ src/App.js
@@ -1,69 +1,25 @@
 import { ChakraProvider, VStack } from "@chakra-ui/react";
 
-import { useState } from "react";
 import { InputTodo } from "./components/InputTodo";
 import { CountBox } from "./components/CountBox";
 import { TodoItems } from "./components/TodoItems";
+import { useTodoManager } from "./hooks/useTodoManager";
 
 function App() {
-  const [todoText, setTodoText] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [id, setId] = useState(1);
-  const [deletingId, setDeletingId] = useState();
+  const {
+    todos,
+    todoText,
+    onChangeText,
+    onClickAddTodo,
+    onToggleComplete,
+    onChangeUpdateText,
+    onClickMemorizeDeletingId,
+    onClickDeleteTodo,
+    onClickToggleUpdate,
+  } = useTodoManager();
+
   const incompleteTodos = todos.filter((item) => !item.isChecked);
   const completeTodos = todos.filter((item) => item.isChecked);
-
-  const onChangeText = (e) => setTodoText(e.target.value);
-
-  const onClickAddTodo = () => {
-    if (todoText === "") return;
-    setTodos([
-      ...todos,
-      { id: id, text: todoText, isEditable: false, isChecked: false },
-    ]);
-    setTodoText("");
-    setId(id + 1);
-  };
-
-  const onToggleComplete = (id) => {
-    const newTodos = todos.map((todo) => {
-      if (id === todo.id) {
-        return { ...todo, isChecked: !todo.isChecked };
-      } else {
-        return todo;
-      }
-    });
-    setTodos([...newTodos]);
-  };
-
-  const onChangeUpdateText = (e, id) => {
-    const newTodos = todos.map((todo) => {
-      if (id === todo.id) {
-        return { ...todo, text: e.target.value };
-      } else {
-        return todo;
-      }
-    });
-    setTodos([...newTodos]);
-  };
-
-  const onClickMemorizeDeletingId = (id) => setDeletingId(id);
-
-  const onClickDeleteTodo = () => {
-    const newTodos = todos.filter((todo) => deletingId !== todo.id);
-    setTodos(newTodos);
-  };
-
-  const onClickToggleUpdate = (id) => {
-    const newTodos = todos.map((todo) => {
-      if (id === todo.id) {
-        return { ...todo, isEditable: !todo.isEditable };
-      } else {
-        return todo;
-      }
-    });
-    setTodos([...newTodos]);
-  };
 
   return (
     <ChakraProvider>
